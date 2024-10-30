9083028bda23c4c6c2cfb1633ab270dbe13cda1c
sandyy221 <kurniawansandy808.com>
2024-10-30 21:33:01 +0700

    tugas1

--- tugas27_inputN5.java
+++ tugas27_inputN5.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
+
 public class tugas27_inputN5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
+
         System.out.print("Masukan nilai N = ");
         int N = sc.nextInt();
-            for (int i = 1; i <= 5; i++) {
-                System.out.print(N);
-            }
-            System.out.println();
-    
+
+        for (int j = 1; j <= 5; j++) {
             System.out.print(N);
-            for (int i = 1; i < 4; i++) {
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(N); 
+            for (int j = 1; j < 4; j++) {
                 System.out.print(" ");
             }
             System.out.print(N);
             System.out.println();
-    
-            for (int i = 1; i <= 5; i++) {
-                System.out.print(N);
-            }
-          System.out.println();
+        }
 
-  }
+        for (int j = 1; j <= 5; j++) {
+            System.out.print(N);
+        }
+        System.out.println();
+    }
 }
